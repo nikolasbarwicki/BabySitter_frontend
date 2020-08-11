c7290c7b3706d2ed48fbcb1d1843b2cc134da9f4
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-08-11 12:12:38 +0200

    Add Login and Register components

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Login = () => <div className="container">LOGIN</div>;
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-export default Login;
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Login success!');
+  };
+
+  return (
+    <div className="container">
+      <div className="row vh-100 d-flex justify-content-center align-items-center">
+        <div className="card col-12 col-md-8 col-lg-6 col-xl-4">
+          <div className="card-body">
+            <h4 className="text-center mb-3">Log in</h4>
+            <form className="form" onSubmit={(e) => onSubmit(e)}>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+
+              <div className="col-12">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block mb-3"
+                >
+                  Log in
+                </button>
+              </div>
+              <hr className="mt-2 mb-3" />
+              <div className="d-inline-flex col-12 justify-content-center">
+                <p className="mr-1 font-weight-bold">
+                  Don&apos;t have an account?
+                </p>
+                <Link to="/register" className="font-weight-bold">
+                  Sign up
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Register = () => <div className="container">REGISTER</div>;
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    role: 'sitter',
+    password: '',
+    password2: '',
+  });
+
+  const { name, surname, email, role, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log('Sign up success!');
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="row vh-100 d-flex justify-content-center align-items-center">
+        <div className="card col-12 col-md-8 col-lg-6 col-xl-4">
+          <div className="card-body">
+            <h4 className="text-center mb-3">Sign up</h4>
+            <form onSubmit={(e) => onSubmit(e)}>
+              <div className="row mb-3">
+                <div className="col">
+                  <label htmlFor="firstName" className="form-label">
+                    First name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="First name"
+                    value={name}
+                    onChange={(e) => onChange(e)}
+                  />
+                </div>
+                <div className="col">
+                  <label htmlFor="firstName" className="form-label">
+                    Surname
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="surname"
+                    placeholder="Surname"
+                    value={surname}
+                    onChange={(e) => onChange(e)}
+                  />
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="role" className="form-label">
+                  Role
+                </label>
+                <select
+                  id="role"
+                  className="form-select"
+                  value={role}
+                  onChange={(e) => onChange(e)}
+                >
+                  <option value="sitter">Babysitter</option>
+                  <option value="parent">Parent</option>
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password2" className="form-label">
+                  Repeat password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password2"
+                  placeholder="Repeat password"
+                  value={password2}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+              <div className="col-12">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block mb-3"
+                >
+                  Sign Up
+                </button>
+              </div>
+              <hr className="mt-2 mb-3" />
+              <div className="d-inline-flex col-12 justify-content-center">
+                <p className="mr-1 font-weight-bold">
+                  Already have an account?
+                </p>
+                <Link to="/login" className="font-weight-bold">
+                  Log in
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
